b2ed7a44b37a8993131bea8b741ae37f73f9b11f
Pranay <pranaygumashta@gmail.com>
2025-07-10 23:21:28 +0530

    Updating viewport

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -8,7 +8,7 @@ const HeroPic = () => {
     <motion.div variants={fadeIn('left', 0.2)} initial="hidden" whileInView="show" viewport={{once:true, amount:0}} className='h-full flex items-center justify-center'>
         <img src="images/my-image.png" alt='Pranay Gumashta' className='max-h-[450px] w-auto'/>
         <div className='absolute -z-10 flex justify-center items-center animate-pulse'>
-            <PiHexagonThin className='md:h-[90%] h-[120%] min-h-[620px] w-auto text-cyan-500 blur-md animate-[spin_20s_linear_infinite]'/>
+            <PiHexagonThin className='md:h-[90%] lg:h-[120%] lg:min-h-[620px] md:min-h-[620px] w-auto text-cyan-500 blur-md animate-[spin_20s_linear_infinite]'/>
         </div>
     </motion.div>
   )
--- src/components/heroSection/SubHeroSection.jsx
+++ src/components/heroSection/SubHeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SubHeroSection = () => {
   return (
-    <div className='w-full border-y border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl md:text-2xl text-4xl py-8 items-center gap-4 bg-brown'>
+    <div className='w-full border-y border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl md:text-2xl text-3xl py-8 items-center gap-4 bg-brown'>
         <p className='md:block hidden'>Adaptive Learner</p>
         <p className='md:block hidden'>Collaborative Thinker</p>
         <p>Precision Minded</p>
